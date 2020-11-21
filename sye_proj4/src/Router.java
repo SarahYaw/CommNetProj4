24b0a873b289a41b32cadccbdbfe6b13d2615234
sarahyaw <syaw@emich.edu>
2020-11-21 14:49:25 -0500

    base population
    have populated the routing matrix w basic info

--- sye_proj4/src/Router.java
+++ sye_proj4/src/Router.java
@@ -8,11 +8,15 @@ public class Router
 {
     public static void main(String[] args)
     {
+        //declare variables
         boolean hasInput=false, hasOutput=false;
-        int inputIndex=0, outputIndex=0;
+        int inputIndex=0, outputIndex=0, nodes=0;
         File input, output;
         FileWriter out;
         Scanner in;
+        String[] paths, table;
+
+        //startup argument handling
         if(args.length>0)
         {
             //check to see what command is input by user
@@ -60,7 +64,7 @@ public class Router
                 System.out.println("output is: sye_input.txt");
             }
         }
-        else 
+        else //if no arguements
         {
             input = new File("sye_input.txt");
             try{}
@@ -75,7 +79,6 @@ public class Router
                     ex.printStackTrace();
                 }
             }
-            System.out.println("input is: sye_input.txt");
             
             output = new File("sye_output.txt");
             try
@@ -83,6 +86,9 @@ public class Router
                 out = new FileWriter(output);
                 in = new Scanner(input);
                 String line = in.nextLine();
+                String intake="";
+
+                //taking in input file paths
                 while (in.hasNextLine())
                 {
                     String temp[] = line.split("\\s");
@@ -92,19 +98,101 @@ public class Router
                         if(!temp[i].isEmpty())
                             lne+=temp[i]+" ";
                     }
-                    System.out.println("line: "+lne);
+                    intake+=lne+"-";
                     line = in.nextLine();
                 }
+                String temp[] = line.split("\\s");
+                String lne="";
+                for(int i=0; i<temp.length; i++)
+                {
+                    if(!temp[i].isEmpty())
+                        lne+=temp[i]+" ";
+                }
+                intake+=lne+"-";
+
+                //put those paths into an array
+                paths = intake.split("-");
+
+                //displays input file V
+                //for(int i = 0; i<paths.length; i++)
+                    //System.out.println(paths[i]);
+
+                //make a list of all of the nodes
+                String nodeList="";
+                for(int i=0; i<paths.length; i++)
+                {
+                    String[] oneNode = nodeList.split(" ");
+                    boolean isIn = false;
+                    String add="";
+                    for(int j = 0; j<oneNode.length; j++)
+                    {
+                        String[] onePath = paths[i].split(" ");
+                        if(onePath[0].equals(oneNode[j]))
+                            isIn=true;
+                        else
+                            add=onePath[0];
+                    }
+                    if(!isIn)
+                        nodeList+=add+" ";
+                    
+                    isIn = false;
+                    add="";
+                    for(int j = 0; j<oneNode.length; j++)
+                    {
+                        String[] onePath = paths[i].split(" ");
+                        if(onePath[1].equals(oneNode[j]))
+                            isIn=true;
+                        else
+                            add=onePath[1];
+                    }
+                    if(!isIn)
+                        nodeList+=add+" ";
+                }
+                
+                //alphabetize node list
+                String[] tempo = nodeList.split(" ");
+                Arrays.sort(tempo);
+                nodeList = String.join(" ",tempo);
+                //System.out.println(nodeList);
+
+                //create & initialize matrix
+                String[][] matrix = new String[tempo.length+2][tempo.length+2];
+                matrix[0][0]=" ";
+                for(int i = 0; i<tempo.length; i++)
+                    matrix[0][i+1]=tempo[i];
+                for(int i = 0; i<tempo.length; i++)
+                    matrix[i+1][0]=tempo[i];
+                for(int i=0; i<tempo.length; i++)
+                    matrix[i+1][i+1]="-";
+
+                //place initial values into matrix
+                for(int i = 0; i<paths.length; i++)
+                {
+                    String[] onePath = paths[i].split(" ");
+                    for(int j = 1; j<paths.length; j++)
+                        if(onePath[0].equals(matrix[0][j]))
+                            for(int k = 0; k<paths.length; k++)
+                                if(onePath[1].equals(matrix[0][k]))
+                                    matrix[j][k]=onePath[1]+","+onePath[2];
+                }
+
+                //display matrix
+                for(int i = 0; i<=tempo.length; i++)
+                {
+                    for(int j = 0; j<=tempo.length; j++)
+                        System.out.print(" "+matrix[i][j]+"\t");
+                    System.out.println("");
+                }
+
                 out.write("hi");
                 out.flush();
                 out.close();
             }
             catch(Exception exe)
             {
-                System.out.println("No File");
+                exe.printStackTrace();
                 System.exit(0);
             }
-            System.out.println("output is: sye_input.txt");
         }
         System.out.println("All Done---------------------");
     }
